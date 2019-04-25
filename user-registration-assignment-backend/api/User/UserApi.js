0824fa4a4a340400b8691bb560abf5256d084d7c
Sharad Padghan <SP2410@ExpertGS.Com>
2019-04-25 17:43:12 +0530

    initial commit

--- user-registration-assignment-backend/api/User/UserApi.js
+++ user-registration-assignment-backend/api/User/UserApi.js
@@ -1,16 +1,16 @@
-var HospitalDbModel = require('./api/Hospital/HospitalDbModel.js')
+var UserDbModel = require('./api/User/UserDbModel.js')
 app.post('/saveUserDetails', function (request, response, next) {
-    HospitalDbModel.saveUserDetails(client, response, request.body, next)
+    UserDbModel.saveUserDetails(client, response, request.body, next)
 })
 
 app.post('/updateUserDetails', function (request, response, next) {
-    HospitalDbModel.updateUserDetails(client, response, request.body, next)
+    UserDbModel.updateUserDetails(client, response, request.body, next)
 })
 
 app.get('/getUsers', function (request, response, next) {
-    HospitalDbModel.getUsers(client, response, request.body, next)
+    UserDbModel.getUsers(client, response, request.body, next)
 })
 
 app.delete('/deleteUser/:id', function (request, response, next) {
-    HospitalDbModel.deleteUser(client, response, request.params.id, next)
+    UserDbModel.deleteUser(client, response, request.params.id, next)
 })
